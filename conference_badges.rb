45092e8cf619980f5f82ca36dd1a3ffb080765be
Patricia Carter <patricia4194@gmail.com>
2018-08-07 22:13:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  badge_array = []
+  name_array.each do |name|
+    badge_array << badge_maker(name)
+  end 
+  badge_array
+end 
+
+def assign_rooms(name_array)
+  room_array = []
+  name_array.each_with_index |name, index|
+    room_array << "Hello, #{name}! You'll be assigned to room #{index+1}."
+  end
+  room_array
+end 
+
+def printer(badge_array, room_array)
+  name_array.each |badge|
+    puts badge
+  end 
+  
+  room_array.each |room|
+    puts room
+  end 
+end 
